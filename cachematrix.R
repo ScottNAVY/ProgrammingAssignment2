7916374aa453f7c61b8625c767b3e44752cdfc17
ScottNAVY <48037700+ScottNAVY@users.noreply.github.com>
2019-07-13 16:12:29 -0400

    Updated Cache Matrix
    Edited the example file to go from a mean of a list to an inverse of a function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## First, we do the work and store it in a Matrix so we can access it later
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<<-NULL
+  set<-function(y){
+      x<<-y
+      m<<-NULL
+  }
+  get<- function() x
+  setInverse<- function(solve) m <<- inverse 
+  getInverse<- function()m
+  list(set = set, get =get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Now we summon the cached data, if it's there
 
 cacheSolve <- function(x, ...) {
+  m<- x$getInverse()
+  if (!is.null(m)) {
+    message("We got the data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setInverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
+
